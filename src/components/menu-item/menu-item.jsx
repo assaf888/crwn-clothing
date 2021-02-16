3c6dc161f620c0e8250449216ac624b03589cd2c
assaf888 <assafscf@gmail.com>
2021-02-16 15:06:00 +0200

    broke homepage into smaller components

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -5,9 +5,11 @@ import '../menu-item/menu-item.scss';
 const MenuItem = ({title, imageUrl, size}) => {
     return (
         <div className={`${size} menu-item`}>
-            <div className="background-image" style={{
+            <div
+                className="background-image" style={{
                 backgroundImage: `url(${imageUrl})`
-            }} />
+            }} 
+            />
             <div className="content">
                 <h1 className="title">{title.toUpperCase() }</h1>
                 <span className="subtitle">SHOP NOW</span>
