c4621faa0722349bf5a34562109cec8d9caa9fb5
assaf888 <assafscf@gmail.com>
2021-03-18 10:40:53 +0200

    updating our app

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -6,11 +6,13 @@ import './cart-icon.scss';
 
 const CartIcon = () => {
 
+    const { cartItems } = useSelector(state => state.cart)
     const dispatch = useDispatch()
+    const itemCount = cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
     return ( 
         <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{itemCount}</span>
         </div>
      );
 }
